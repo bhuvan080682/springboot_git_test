e5ada657e9da5ecf27c35708de69ff84c962ba9d
yateesh <bhugee@gmail.com>
2019-01-05 15:10:12 +0530

    initail commit

---
+++ src/main/java/com/bhuni/git/git_spring_test/GitSpringTestApplication.java
@@ -0,0 +1,14 @@
+package com.bhuni.git.git_spring_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitSpringTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitSpringTestApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/bhuni/git/git_spring_test/GitSpringTestApplicationTests.java
@@ -0,0 +1,17 @@
+package com.bhuni.git.git_spring_test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GitSpringTestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
