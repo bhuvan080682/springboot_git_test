7715e11b4778e647c026809f641a74c445d90c97
yateesh <bhugee@gmail.com>
2019-01-05 20:53:42 +0530

    added greeting service

--- src/main/java/com/bhuni/git/git_spring_test/controller/GreetingController.java
+++ src/main/java/com/bhuni/git/git_spring_test/controller/GreetingController.java
@@ -1,14 +1,19 @@
 package com.bhuni.git.git_spring_test.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.bhuni.git.git_spring_test.service.GreetingService;
+
 @RequestMapping("greetings")
 public class GreetingController {
+	@Autowired
+	private GreetingService service;
 	
 	@GetMapping
 	public String helloGreeting(){
-		return "hello";
+		return service.display();
 	}
 
 }
